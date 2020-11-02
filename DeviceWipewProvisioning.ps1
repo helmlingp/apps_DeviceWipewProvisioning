737fed6458f53987e03e311f971703e8483e6bff
helmlingp <66004946+helmlingp@users.noreply.github.com>
2020-11-02 15:45:19 +1030

    Update DeviceWipewProvisioning.ps1
    added plain Device Wipe capabilities

--- DeviceWipewProvisioning.ps1
+++ DeviceWipewProvisioning.ps1
@@ -7,12 +7,12 @@
 	  Organization:   VMware, Inc.
 	  Filename:       DeviceWipeWProvisioning.ps1
 	.DESCRIPTION
-    Reapplies Workspace ONE Factory Provisioning PPKG and unattend.xml at Device Reset
-    Copies files to C:\Recovery\OEM folder to be used by Push Button Reset (Device Wipe) process.
+    Reapplies Workspace ONE Factory Provisioning PPKG and unattend.xml at Device Reset and
+    Device Reset with Provisioning Data. 
+    Copies files to C:\Recovery\OEM and C:\Recovery\AutoApply folder to be used by Push Button Reset (Device Wipe) process.
     
-    Include Workspace ONE Factory Provisioning unattend.xml used for original deployment
+    Requires the inclusion of Workspace ONE Factory Provisioning unattend.xml used for original deployment
     in package folder before ZIP'ing. Unattend.xml can be called anything.xml
-    
     No other modifications required to this package.
 
     Install command: powershell.exe -ep bypass -file .\DeviceWipeWProvisioning.ps1
@@ -30,6 +30,7 @@ if($PSScriptRoot -eq ""){
 }
 
 $OEMPATH = "C:\Recovery\OEM"
+$AUTOAPPLY = "C:\Recovery\AutoApply"
 $unattend = Get-ChildItem -Path $current_path -Include *.xml* -Recurse -Exclude ResetConfig.xml -ErrorAction SilentlyContinue
 
 if(!$unattend){
@@ -37,11 +38,18 @@ if(!$unattend){
   Write-Host "Cannot find unattend.xml"
 } else {
   $OEMPATHExists = Get-Item -Path $OEMPATH
+  $AUTOAPPLYExists = Get-Item -Path $AUTOAPPLY
   if($OEMPATHExists){
     Write-Host "Copying files"
     Copy-Item -Path $unattend -Destination "$OEMPATH\AWResetUnattend.xml" -Force
     Copy-Item -Path "$current_path\ResetConfig.xml" -Destination $OEMPATH -Force
     Copy-Item -Path "$current_path\VMwareResetRecover.cmd" -Destination $OEMPATH -Force
+    if($AUTOAPPLYExists){
+      Copy-Item -Path $unattend -Destination "$AUTOAPPLY\unattend.xml" -Force
+    } Else {
+      New-Item -Path $AUTOAPPLY -ItemType Directory -Force
+      Copy-Item -Path $unattend -Destination "$AUTOAPPLY\unattend.xml" -Force
+    }
   } Else {
     #fail
     Write-Host "Cannot find C:\Recovery\OEM folder, no Press Button Reset available on this machine."
