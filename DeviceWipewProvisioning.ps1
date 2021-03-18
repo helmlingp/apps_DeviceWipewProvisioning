f8c9cda11ff7069dfeaee179648033a4565b0929
helmlingp <66004946+helmlingp@users.noreply.github.com>
2021-03-18 15:10:35 +1030

    Update DeviceWipewProvisioning.ps1

--- DeviceWipewProvisioning.ps1
+++ DeviceWipewProvisioning.ps1
@@ -6,12 +6,30 @@
 	  Created by:	    Phil Helmling, @philhelmling
 	  Organization:   VMware, Inc.
 	  Filename:       DeviceWipeWProvisioning.ps1
+    Updated:        March, 2021
 	.DESCRIPTION
     Reapplies Workspace ONE Factory Provisioning PPKG and unattend.xml at Device Reset with Provisioning Data.
-    Copies files to C:\Recovery\OEM folder to be used by Push Button Reset (Device Wipe) process.
+    Copies files included in this package to C:\Recovery\OEM folder to be used by Push Button Reset (Device Wipe) process.
     
-    Include Workspace ONE Factory Provisioning unattend.xml used for original deployment
-    in package folder before ZIP'ing. Unattend.xml can be called anything, eg myunatten.xml.
+    *New - add capability to deploy to 'brownfield' Windows 10 device to provide over-the-air rebuild to a 'known good state'
+    It does this by copying the PPKG file included in this package to C:\Recovery\Customizations
+    This option can be used in conjunction with Agent Only Enrolment flow, eg. AirLift SCCM Migration & Enrolment
+    This option will overwrite the existing PPKG.
+
+    REQUIREMENTS:
+    1. WS1 Intelligent Hub (AirwatchAgent.msi) installed and Device enrolled
+    2. WinRE partition on the device with Windows RE boot image (Winre.wim) available on the local hard drive
+    https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/pbr-faq
+    
+    IMPORTANT:
+    1. Please include the Workspace ONE Factory Provisioning unattend.xml used for original deployment in package folder before ZIP'ing.
+    2. Include only one XML file in the package folder.
+    3. Unattend.xml can be called anything, eg myunatten.xml.
+    
+    OPTIONAL Brownfield Devices:
+    1. Include Workspace ONE Factory Provisioning Package file (PPKG) used for original deployment in package folder before ZIP'ing.
+    2. Include only one PPKG file in the package folder.
+    3. PPKG can be called anything, eg. myprovisioningpackage.ppkg.
     
     No other modifications required to this package.
 
@@ -53,14 +71,25 @@ if(!$unattend){
     Copy-Item -Path "$current_path\VMwareResetRecover.cmd" -Destination $OEMPATH -Force
   }
 }
-#  $OEMPATHExists = Get-Item -Path $OEMPATH
-<#   if($OEMPATHExists){
+
+$CustomPATH = "C:\Recovery\Customizations"
+$PPKG = Get-ChildItem -Path $current_path -Include *.ppkg* -Recurse -ErrorAction SilentlyContinue
+if(!$PPKG){
+  #Don't do anything
+} else {
+  if (-not (Test-Path -LiteralPath $CustomPATH)) {
+    
+    try {
+        New-Item -Path $CustomPATH -ItemType Directory -ErrorAction Stop | Out-Null #-Force
+    }
+    catch {
+        Write-Error -Message "Unable to create directory '$CustomPATH'. Error was: $_" -ErrorAction Stop
+    }
+    "Successfully created directory '$CustomPATH'."
+  } else {
+    Write-host "Removing existing PPKG file"
+    Remove-Item -Path $CustomPATH -Include *.ppkg* -Recurse -ErrorAction SilentlyContinue
     Write-Host "Copying files"
-    Copy-Item -Path $unattend -Destination "$OEMPATH\AWResetUnattend.xml" -Force
-    Copy-Item -Path "$current_path\ResetConfig.xml" -Destination $OEMPATH -Force
-    Copy-Item -Path "$current_path\VMwareResetRecover.cmd" -Destination $OEMPATH -Force
-  } Else {
-    #fail
-    Write-Host "Cannot find C:\Recovery\OEM folder, no Press Button Reset available on this machine."
+    Copy-Item -Path $PPKG -Destination $CustomPATH -Force
   }
-} #>
+}
